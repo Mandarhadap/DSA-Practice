1b70dbc2ae2ad3ec3aa1b4808acc27d114c72956
Mandar <mandar.hadap5@gmail.com>
2026-02-15 01:03:33 +0530

    RemoveDuplicatesSorted

---
+++ ArrayList/removeDublicatesSorted.java
@@ -0,0 +1,36 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class removeDublicatesSorted{
+
+    public static List<Integer> removeDublicates(int[] arr){
+
+        List<Integer> list = new ArrayList<>();
+
+        //Edge Case if the array is null and empty
+
+        if(arr == null || arr.length == 0){
+            return list;
+        }
+
+        // add the first element that is always unique
+
+        list.add(arr[0]);
+
+        for(int i = 1 ; i < arr.length ; i++){
+            if(arr[i] != arr[i-1]){
+                list.add(arr[i]);
+            }
+        }
+        return list;
+    }
+    public static void main (String[] args){
+        int[] arr = {1,1,2,3,3,4,4,5,5,6};
+        List<Integer> result = removeDublicates(arr);
+        System.out.println(result);
+    }
+
+
+}
